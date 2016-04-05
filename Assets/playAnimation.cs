cac4aa895a88442ad9d1bbba542056b55044b015
jjunior776 <juniorn@seznam.cz>
2016-04-05 21:11:42 +0200

    Repository created

---
+++ Assets/playAnimation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playAnimation : MonoBehaviour {
+
+    public Animator anim;
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update() {
+
+        if (Input.GetKey(KeyCode.Alpha1)) {
+            anim.SetInteger("animation", 1);
+        }
+        if (Input.GetKey(KeyCode.Alpha2)) {
+            anim.SetInteger("animation", 2);
+        }
+        if (Input.GetKey(KeyCode.Alpha3)) {
+            anim.SetInteger("animation", 3);
+        }
+
+    }
+    void setIdle() {
+        anim.SetInteger("animation", 0);
+    }
+}
